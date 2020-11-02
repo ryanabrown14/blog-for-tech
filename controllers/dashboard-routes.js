72162147ebe6d506017fff09717385e275f60b5e
ry brown <ryan.alex.brown@gmail.com>
2020-11-01 19:30:44 -0700

    back end complete

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -20,4 +20,23 @@ router.get("/", withAuth, (req, res) => {
     });
 });
 
+router.get("/edit/:id", withAuth, (req, res) => {
+    Post.findByPk(req.params.id)
+      .then(dbPostData => {
+        if (dbPostData) {
+          const post = dbPostData.get({ plain: true });
+          
+          res.render("edit-post", {
+            layout: "dashboard",
+            post
+          });
+        } else {
+          res.status(404).end();
+        }
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+});
+
 module.exports = router;
